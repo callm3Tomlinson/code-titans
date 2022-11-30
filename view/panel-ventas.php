c6a6fda0e55e86640a0d66eb53b135018af60151
callm3Tomlinson <charlesarthur1913@gmail.com>
2022-11-30 13:35:50 -0500

    Arreglando include en panel-ventas

--- view/panel-ventas.php
+++ view/panel-ventas.php
@@ -1,7 +1,7 @@
 <?php
 
     include_once 'templates/header.php';
-    include_once '../controller/ventaController';
+    include_once 'controller/ventaController.php';
     $nombreusuario = $_SESSION["user"]; 
 
 ?>
